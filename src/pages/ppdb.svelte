d3a0a9d1a54df4866bd0490b13addc65f349d50d
mzaini30 <muhzaini30@gmail.com>
2023-08-26 20:14:25 +0800

    upload dulu yang ada

--- src/pages/ppdb.svelte
+++ src/pages/ppdb.svelte
@@ -1,11 +1,19 @@
 <script>
-    import { halaman_aktif } from "../state/tombol-aktif";
+  import { halaman_aktif } from "../state/tombol-aktif";
 
-$halaman_aktif = "ppdb"
+  $halaman_aktif = "ppdb";
 </script>
 
-<iframe
+<div class="p-3">
+  <a
+    class="rounded p-2 bg-green-500 text-white w-full block text-center"
+    href="https://docs.google.com/forms/d/e/1FAIpQLSdnP4SPAuU-OckkG_rc6Q89iqw1ZU3F0tzwF3HT-brI3bDimg/viewform"
+    >Pendaftaran Siswa Baru</a
+  >
+</div>
+
+<!-- <iframe
   class="w-full min-h-100"
-  src="https://docs.google.com/forms/d/e/1FAIpQLSdzUz5_YuK1s_TbELQDpzFLVWkRKgo3Asro6O0_lNxjHRT8ag/viewform"
+  src="https://docs.google.com/forms/d/e/1FAIpQLSdnP4SPAuU-OckkG_rc6Q89iqw1ZU3F0tzwF3HT-brI3bDimg/viewform"
   frameborder="0"
-></iframe>
+></iframe> -->
--- src/pages/profil.svelte
+++ src/pages/profil.svelte
@@ -1,5 +1,14 @@
 <script>
   import { halaman_aktif } from "../state/tombol-aktif";
 
+  let kode_unik;
+
+  kode_unik = crypto.randomUUID().split("-")[0];
+
   $halaman_aktif = "profil";
 </script>
+
+<div class="px-2">
+  <p>Kode unik</p>
+  <p>{kode_unik}</p>
+</div>
