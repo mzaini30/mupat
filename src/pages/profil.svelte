c7d3f70803f4b2a1bc3baadd768856a5f5b30721
mzaini30 <muhzaini30@gmail.com>
2023-09-03 16:38:40 +0800

    belum ada perubahan

--- src/pages/profil.svelte
+++ src/pages/profil.svelte
@@ -2,6 +2,8 @@
   import { halaman_aktif } from "../state/tombol-aktif";
 
   let kode_unik;
+  let id = "0ce5bba79b60d7fed796f33ec9cb9080"; // id, nama, username, password, kode_unik
+  // https://sdmuh4t-smr.sch.id/excalibur/scubus-index.php?halaman=olah-sql&database=0ce5bba79b60d7fed796f33ec9cb9080
 
   kode_unik = crypto.randomUUID().split("-")[0];
 
