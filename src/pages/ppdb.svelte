2a7678308df0bb7ae81a0a0ea4534b7d9509edf9
mzaini30 <muhzaini30@gmail.com>
2023-09-06 15:58:24 +0800

    kirim

--- src/pages/ppdb.svelte
+++ src/pages/ppdb.svelte
@@ -24,7 +24,7 @@
   </p>
   <a
     class="rounded p-2 bg-green-500 text-white w-full block text-center"
-    href="https://docs.google.com/forms/d/e/1FAIpQLSdnP4SPAuU-OckkG_rc6Q89iqw1ZU3F0tzwF3HT-brI3bDimg/viewform"
+    href="#/form"
     >Pendaftaran Siswa Baru</a
   >
 </div>
