f69ec52e712761f1705a2715a2302a4259b49bb3
mzaini30 <muhzaini30@gmail.com>
2024-08-08 15:44:12 +0800

    upload

--- src/pages/ppdb.svelte
+++ src/pages/ppdb.svelte
@@ -2,15 +2,27 @@
   import { halaman_aktif } from "../state/tombol-aktif";
   import gosit from "gosit";
   import TampilkanPostingan from "../komponen/tampilkan-postingan.svelte";
+  import excalibur from "excalibur-zen";
+  import api from "../data/api.json";
 
   let banyaknya_yang_mendaftar = 0;
 
   $halaman_aktif = "ppdb";
 
   async function run() {
+    let datanya = await excalibur(api.server, {
+      id: api.spreadsheet,
+      kunci: "ambil",
+    });
+    datanya = await datanya.json();
+    console.log(datanya);
+    // let result = await gosit(
+    //   "1S_-lKdbmroS6h1LJg9_9oVqwPLHl5R_TuZzrfeIdlio", // spreadsheet id
+    //   "Form Responses 1", // sheet name
+    // );
     let result = await gosit(
-      "1S_-lKdbmroS6h1LJg9_9oVqwPLHl5R_TuZzrfeIdlio", // spreadsheet id
-      "Form Responses 1", // sheet name
+      datanya[0].spreadsheet_id, // spreadsheet id
+      datanya[0].sheet_name, // sheet name
     );
     // console.log(result);
     banyaknya_yang_mendaftar = result.length;
